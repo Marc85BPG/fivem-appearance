651aa91161995944ac27cc3e0a7a7a71adf52240
Prometheus <m4rcyt85@gmail.com>
2023-01-08 01:54:27 +0100

    Create strings_de.lua

---
+++ configuration/strings_de.lua
@@ -0,0 +1,34 @@
+Strings = {
+    --Text UI strings
+    clothing_menu = '[E] - Angebot ansehen',
+    barber_menu = '[E] - Den Friseur besuchen',
+    tattoo_menu = '[E] - Tattookatalog durchschauen',
+
+    --Notifications
+    success = 'Erfolgreich',
+    success_desc = 'Du hast %s $ bezahlt!',
+    no_funds = 'Nicht genug Geld',
+    no_funds_desc = 'Du hast nicht genug Bargeld dabei!',
+
+    --Main clothing shop menu
+    clothing_shop_title = 'Bekleidungsladen',
+    change_clothing_title = 'Kleidungsangebot',
+    change_clothing_desc = 'Stöber durch das Angebot',
+    browse_outfits_title = 'Hinterlegte Outfits',
+    save_outfit_info = 'Outfit Name',
+    browse_outfits_desc = 'Deine hinterlegten Outfits ansehen',
+    save_outfit_title = 'Outfit hinterlegen',
+    save_outfit_desc = 'Speichere ein Outfit für deinen Kleiderschrank',
+    delete_outfit_title = 'Outfit entfernen',
+    delete_outfit_desc = 'Räume deinen Kleiderschrank auf',
+
+    --Browse/Delete outfits & Wardrobe
+    wardrobe_title = 'Kleiderschrank',
+    delete_outfits_title = 'Kleiderschrank aufräumen',
+    go_back_title = '< Zurück',
+    go_back_desc = 'Keine hinterlegten Outfits!',
+
+    --Other
+    skin_command_help = 'Ändere Ped',
+    skin_command_arg_help = 'Spieler ID'
+}
